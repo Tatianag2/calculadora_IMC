c70bb249c70c6cf213181aeaa89c996693d9ac62
Tatianag2 <tatiana3g2@gmail.com>
2021-11-27 02:07:11 -0500

    almacenamiento localStorage y estadisticas

--- script/script.js
+++ script/script.js
@@ -19,10 +19,15 @@ if(document.getElementById("female")){
 }
 
 function validaciones(){
+    let sexo = "";
+    if(document.getElementById("male").style.color == 'gold'){
+        sexo = "Hombre"
+    } else if(document.getElementById("female").style.color == 'gold'){
+        sexo = "Mujer"
+    }
     let edad = document.getElementById("inputEdad").value;
     let peso = document.getElementById("inputPeso").value;
     let altura = document.getElementById("inputAltura").value;
-    let alerta = document.getElementById("alerta");
 
     if(male.style.color != 'gold' & female.style.color != 'gold'){
         alert("Seleccione por favor su sexo")
@@ -37,49 +42,132 @@ function validaciones(){
     }else if(edad < 16){
         alert("Debe ser mayor de 16 años")
     }else {
-        capturarDatos();
+        capturarDatos(sexo, edad, peso, altura);
     }
 }
 
-function obtenerLocalStorage(){
-    let datos = JSON.parse(localStorage.getItem('Datos'));
-    let imc = datos.peso / (datos.altura**2);
-    let pesoIdealMin = (18.5 * (datos.altura**2)).toFixed(2);
-    let pesoIdealMax = (24.9 * (datos.altura**2)).toFixed(2);
-    let fragment = document.createDocumentFragment();
-    let rangoPeso = document.getElementById("rangoPeso");
+let data = [];
 
-    document.getElementById("inputResultado").value = imc.toFixed(2);
-
-    let div = document.createElement("div");
-    div.innerHTML = `<p id="rangoPesoIdeal">Peso ideal: ${pesoIdealMin} - ${pesoIdealMax} (Kg)</p>`
-    fragment.appendChild(div);
-    rangoPeso.appendChild(fragment);
-
-    let flechaRango = document.getElementById("flechaRango");
-
-    if(imc<=50){
-        let pixelesLeft = (imc*8).toFixed(2);
-        flechaRango.style.left = `${pixelesLeft}px`;
-    } else {
-        flechaRango.style.left = '400px';
-    }
-}
-
-obtenerLocalStorage();
-
-function capturarDatos(){
-    let edadV = document.getElementById("inputEdad").value;
-    let pesoV = document.getElementById("inputPeso").value;
-    let alturaV = document.getElementById("inputAltura").value;
+function capturarDatos(psexo, pedad, ppeso, paltura){
 
     let objetoDatos = {
-        edad: edadV,
-        peso: pesoV,
-        altura: alturaV,
+        sexo: psexo,
+        edad: pedad,
+        peso: ppeso,
+        altura: paltura
     }
-
-    localStorage.setItem('Datos', JSON.stringify(objetoDatos));
-
+    
+    let data = getListaDatos();
+    data.push(objetoDatos);
+    localStorage.setItem('Datos', JSON.stringify(data));
+    
     document.getElementById("enlace").setAttribute('href', '../pages/resultado.html')
+}
+
+function getListaDatos(){
+    let listaDatosLocal = JSON.parse(localStorage.getItem('Datos'));
+    if(listaDatosLocal == null){
+        listaDatosLocal = data;
+    }
+    
+    return listaDatosLocal;
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    obtenerIMC();
+})
+
+function obtenerIMC(){
+
+    let datosTotales = JSON.parse(localStorage.getItem('Datos'));
+    let datoFinal = datosTotales[datosTotales.length - 1]
+    let imc = datoFinal.peso / (datoFinal.altura**2);
+    let pesoIdealMin = (18.5 * (datoFinal.altura**2)).toFixed(2);
+    let pesoIdealMax = (24.9 * (datoFinal.altura**2)).toFixed(2);
+    let fragment = document.createDocumentFragment();
+    let rangoPeso = document.getElementById("rangoPeso");
+    
+    if(document.getElementById("inputResultado")){
+        document.getElementById("inputResultado").value = imc.toFixed(2);
+
+        let div = document.createElement("div");
+        div.innerHTML = `<p id="rangoPesoIdeal">Peso ideal: ${pesoIdealMin} - ${pesoIdealMax} (Kg)</p>`
+        fragment.appendChild(div);
+        rangoPeso.appendChild(fragment);
+
+        let flechaRango = document.getElementById("flechaRango");
+    
+        if(imc<=50){
+            let pixelesLeft = (imc*8).toFixed(2);
+            flechaRango.style.left = `${pixelesLeft}px`;
+        } else {
+            flechaRango.style.left = '400px';
+        }
+    }
+}
+
+if(document.getElementById("btnEstad")){
+    document.getElementById("btnEstad").addEventListener('click', function(){
+        document.getElementById("btnEstad").style.display = 'none';
+        let datosTotales = JSON.parse(localStorage.getItem('Datos')).reverse();
+        let fragment = document.createDocumentFragment();
+        let tablaIMC = document.getElementById("tablaIMC");
+    
+        let thead = document.createElement("thead");
+        thead.innerHTML = `<thead>
+                                <tr><td>No.</td>
+                                <td>Sexo</td>
+                                <td>Edad</td>
+                                <td>Peso</td>
+                                <td>Altura</td>
+                                <td>IMC</td></tr>
+                                </thead>`
+        fragment.appendChild(thead);
+        tablaIMC.appendChild(fragment);
+    
+        for(let i=1; i<16; i++){
+            let tbody = document.createElement("tbody");
+            
+            tbody.innerHTML = `<tbody id="cuerpoTabla">
+                                <tr><td class="posicion">${i}</td>
+                                <td class="sexoT">${datosTotales[i].sexo}</td>
+                                <td class="edadT">${datosTotales[i].edad}</td>
+                                <td class="pesoT">${datosTotales[i].peso}</td>
+                                <td class="alturaT">${datosTotales[i].altura}</td>
+                                <td class="imcT" id="${datosTotales[i].sexo}">${(datosTotales[i].peso/datosTotales[i].altura**2).toFixed(2)}</td></tr>
+                                </tbody>`;
+            fragment.appendChild(tbody);
+            tablaIMC.appendChild(fragment);
+        }
+    
+        let totalImcHombres = 0;
+        let totalImcMujeres = 0;
+        let imcTabla = document.getElementsByClassName("imcT");
+    
+        for(let x=0; x<imcTabla.length ; x++){
+            if(imcTabla[x].id == 'Mujer'){
+                totalImcMujeres += parseFloat(imcTabla[x].textContent);
+            } else {
+                totalImcHombres += parseFloat(imcTabla[x].textContent);
+            }
+        }
+    
+        let mediaImcHombres = totalImcHombres/imcTabla.length;
+        let mediaImcMujeres = totalImcMujeres/imcTabla.length;
+    
+        let grafica = document.getElementById("grafica").getContext('2d');
+        var chart = new Chart(grafica,{
+            type: 'bar',
+            data:{
+                labels:['Mujeres' , 'Hombres'],
+                datasets:[
+                    {
+                        label: 'Gráfica media imc por sexo',
+                        backgroundColor: ['hsl(127deg 43% 55%)', 'hsl(215deg 66% 55%)'],
+                        data:[mediaImcMujeres, mediaImcHombres]
+                    }
+                ]
+            }
+        })
+    })
 }
